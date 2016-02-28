56e801c077e20e27610367c4c84442366a4f192e
thepractitioner <matthewsweekes@gmail.com>
2016-02-28 18:24:21 -0500

    Added files via upload

---
+++ easychallenge1.js
@@ -0,0 +1,5 @@
+var name = prompt("What is your name?");
+var age = prompt("How old are you?");
+var reddit_Username = prompt("What is your reddit user name?");
+
+consolel.log("Your name is " + name + ", you are " + age " years old, and your user name is " + reddit_Username);
